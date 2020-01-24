1e5a467a9ae17b83ebd9499f046095f00f95a33f
Jakson Lima <jaksonsneider@gmail.com>
2020-01-23 22:35:47 -0300

    Projeto Web Incio parada 48:08 V3

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Global.css';
+import './App.css'
+
+function App() {
+  return (
+    <div id='app'>
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
