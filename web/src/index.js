d828506fbed8a45d36ca505c4ef7bdfc5ad9af0a
Jakson Lima <jaksonsneider@gmail.com>
2020-01-29 20:54:04 -0300

    web mobol

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-//tudo sobre React
-
-//Componete
-//Estado
-//Propriedade 
